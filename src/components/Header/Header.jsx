5340c29c21ce38e1968558ef35f57d0a991af628
Yura <yuraplatonov2000@gmail.com>
2024-08-25 00:28:52 +0300

    fix border

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { NavLink  } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../images/Logo.svg';
 import css from './Header.module.css';
 
 const Header = () => {
+  const location = useLocation();
+
+  let borderBottomClass;
+  switch (location.pathname) {
+    case '/':
+      borderBottomClass = css.borderBottomHome;
+      break;
+    case '/technika':
+      borderBottomClass = css.borderBottomTechnika;
+      break;
+    case '/kariera':
+      borderBottomClass = css.borderBottomKariera;
+      break;
+    default:
+      borderBottomClass = css.borderBottomDefault;
+  } 
+
   return (
     <header className={css.header}>
-    <div className={css.borderBottom}></div>
+    <div className={borderBottomClass}></div>
       <Logo />
       <nav className={css.navigation}>
         <div  className={css.navList}>
