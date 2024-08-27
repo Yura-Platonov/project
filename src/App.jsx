a30c3780d5d29e4ca414de2cfd7cd50eee28ae4e
Yura <yuraplatonov2000@gmail.com>
2024-08-27 15:12:31 +0300

    start making section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from 'react'
+import Modal from 'react-modal'
+import { Route, Routes } from 'react-router-dom'
 // import './index.css';
-import Header from './components/Header/Header';
-import Domy from './pages/Domy/Domy';
-import Kariera from './pages/Kariera/Kariera';
-import Technika from './pages/Technika/Technika';
+import Header from './components/Header/Header'
+import Domy from './pages/Domy/Domy'
+import Kariera from './pages/Kariera/Kariera'
+import Technika from './pages/Technika/Technika'
 
+Modal.setAppElement('#root')
 
 export const App = () => {
-    return (
-    <div className="app">
-      <Header />
-      <Routes>
-        <Route path="/" exact element={<Domy/>} />
-        <Route path="/kariera" element={<Kariera />} />
-        <Route path="/technika" element={<Technika />} />
-      </Routes>
-    </div>
-  );
-};
+	return (
+		<div className='app'>
+			<Header />
+			<Routes>
+				<Route path='/' exact element={<Domy />} />
+				<Route path='/kariera' element={<Kariera />} />
+				<Route path='/technika' element={<Technika />} />
+			</Routes>
+		</div>
+	)
+}
--- src/components/TechnikaFirst/TechnikaFirst.jsx
+++ src/components/TechnikaFirst/TechnikaFirst.jsx
@@ -1,29 +1,80 @@
-import React from 'react';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import css from './TechnikaFirst.module.css'; 
-import image1 from '../../images/Photo1.1.jpg';
-import image2 from '../../images/Photo1.2.jpg';
-import image3 from '../../images/Photo1.3.jpg';
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+import image1 from '../../images/Photo1.1.jpg'
+import image2 from '../../images/Photo1.2.jpg'
+import image3 from '../../images/Photo1.3.jpg'
+import css from './TechnikaFirst.module.css'
+
+const images = [image1, image2, image3]
 
 const TechnikaFirst = () => {
-  
-  
-    return (
-      <div className={css.sliderContainer}>
-        <Splide options={ { rewind: true, pagination: false, } } aria-label="React Splide">
-      <SplideSlide>
-        <img src={image1} alt=" 1" />
-      </SplideSlide>
-      <SplideSlide>
-        <img src={image2}  alt=" 2"/>
-      </SplideSlide>
-      <SplideSlide>
-        <img src={image3}  alt=" 3"/>
-      </SplideSlide>
-    </Splide>
-      </div>
-    );
-  };
-  
-  export default TechnikaFirst;
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+	const openModal = index => {
+		setCurrentImageIndex(index)
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+	}
+
+	const handleNextImage = () => {
+		setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length)
+	}
+
+	const handlePrevImage = () => {
+		setCurrentImageIndex(
+			prevIndex => (prevIndex - 1 + images.length) % images.length
+		)
+	}
+
+	return (
+		<section className={css.technikaSection}>
+			<h2 className={css.technikaTitle}>JCB 3CX KOPARKO-LADOWARKA 2015</h2>
+			<div className={css.sliderContainer}>
+				<Splide
+					options={{ rewind: true, pagination: false }}
+					aria-label='React Splide'
+				>
+					{images.map((image, index) => (
+						<SplideSlide key={index}>
+							<img
+								src={image}
+								alt={`Slide ${index + 1}`}
+								onClick={() => openModal(index)}
+							/>
+						</SplideSlide>
+					))}
+				</Splide>
+
+				<Modal
+					isOpen={isModalOpen}
+					onRequestClose={closeModal}
+					className={css.modal}
+					overlayClassName={css.overlay}
+				>
+					<button onClick={closeModal} className={css.closeButton}>
+						Закрыть
+					</button>
+					<button onClick={handlePrevImage} className={css.navButton}>
+						Назад
+					</button>
+					<img
+						src={images[currentImageIndex]}
+						alt={`Modal Slide ${currentImageIndex + 1}`}
+						className={css.modalImage}
+					/>
+					<button onClick={handleNextImage} className={css.navButton}>
+						Вперед
+					</button>
+				</Modal>
+			</div>
+		</section>
+	)
+}
+
+export default TechnikaFirst
