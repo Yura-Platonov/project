677a17933fc2b3573e6c4efd0f9a2d56ed136040
Yura <yuraplatonov2000@gmail.com>
2024-08-28 09:48:54 +0300

    add section

--- src/components/TechnikaFirst/TechnikaFirst.jsx
+++ src/components/TechnikaFirst/TechnikaFirst.jsx
@@ -50,29 +50,98 @@ const TechnikaFirst = () => {
 						</SplideSlide>
 					))}
 				</Splide>
-
-				<Modal
-					isOpen={isModalOpen}
-					onRequestClose={closeModal}
-					className={css.modal}
-					overlayClassName={css.overlay}
-				>
-					<button onClick={closeModal} className={css.closeButton}>
-						Закрыть
-					</button>
-					<button onClick={handlePrevImage} className={css.navButton}>
-						Назад
-					</button>
-					<img
-						src={images[currentImageIndex]}
-						alt={`Modal Slide ${currentImageIndex + 1}`}
-						className={css.modalImage}
-					/>
-					<button onClick={handleNextImage} className={css.navButton}>
-						Вперед
-					</button>
-				</Modal>
 			</div>
+			<ul className={css.technikaList}>
+				<li className={css.technikaItem}>
+					<h3 className={css.technikaSubTitle}>
+						PARAMETRY ROBOCZE RAMIENIA KOPARKOWEGO
+					</h3>
+					<ul className={css.specificationList}>
+						<li className={css.specificationItem}>
+							sterowanie ramieniem koparkowym za pomocą dźwigni
+						</li>
+						<li className={css.specificationItem}>
+							maksymalna głębokość kopania – 5,97 m
+						</li>
+						<li className={css.specificationItem}>
+							zasięg na poziomie gruntu od osi obrotu ramienia – 6,52 m
+						</li>
+						<li className={css.specificationItem}>
+							zasięg na poziomie gruntu w bok od osi maszyny – 7,09 m
+						</li>
+						<li className={css.specificationItem}>
+							wysokość załadunku – 4,72 m
+						</li>
+						<li className={css.specificationItem}>
+							wysokość załadunku – 4,72 m
+						</li>
+					</ul>
+				</li>
+				<li className={css.technikaItem}>
+					<h3 className={css.technikaSubTitle}>WYMIARY I MASY</h3>
+					<ul className={css.specificationList}>
+						<li className={css.specificationItem}>
+							długość całkowita – 5,62 m
+						</li>
+						<li className={css.specificationItem}>szerokość – 2,35 m</li>
+						<li className={css.specificationItem}>wysokość z kabiną – 3,03m</li>
+						<li className={css.specificationItem}>rozstaw osi – 2,17 m</li>
+						<li className={css.specificationItem}>
+							masa całkowita – 8 600 kg.
+						</li>
+					</ul>
+				</li>
+				<li className={css.technikaItem}>
+					<h3 className={css.technikaSubTitle}>
+						PARAMETRY ROBOCZE RAMIENIA ŁADOWARKOWEGO
+					</h3>
+					<ul className={css.specificationList}>
+						<li className={css.specificationItem}>
+							wysokość załadunku – 3,20 m
+						</li>
+						<li className={css.specificationItem}>
+							wysokość wyładunku – 2,72 m
+						</li>
+						<li className={css.specificationItem}>
+							maksymalny zasięg na pełnej wysokości – 1,37 m
+						</li>
+						<li className={css.specificationItem}>
+							ładunek podnoszony do pełnej wysokości – 3 169 kg
+						</li>
+						<li className={css.specificationItem}>
+							siła zrywania na ramionach łyżki – 4 732 kG
+						</li>
+						<li className={css.specificationItem}>
+							rozstaw wideł – 0,2 – 2,17 m
+						</li>
+						<li className={css.specificationItem}>
+							wysokość podnoszenia na widłach – 2,92 m
+						</li>
+					</ul>
+				</li>
+			</ul>
+
+			<Modal
+				isOpen={isModalOpen}
+				onRequestClose={closeModal}
+				className={css.modal}
+				overlayClassName={css.overlay}
+			>
+				<button onClick={closeModal} className={css.closeButton}>
+					Закрыть
+				</button>
+				<button onClick={handlePrevImage} className={css.navButton}>
+					Назад
+				</button>
+				<img
+					src={images[currentImageIndex]}
+					alt={`Modal Slide ${currentImageIndex + 1}`}
+					className={css.modalImage}
+				/>
+				<button onClick={handleNextImage} className={css.navButton}>
+					Вперед
+				</button>
+			</Modal>
 		</section>
 	)
 }
