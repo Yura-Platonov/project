462cff22e3fb906ddb39db4603083338c46fd8c2
Yura <yuraplatonov2000@gmail.com>
2024-09-20 12:45:17 +0300

    add nieryhomost welcome video

--- src/components/NieruchomośćWelcome/NieruchomośćWelcome.jsx
+++ src/components/NieruchomośćWelcome/NieruchomośćWelcome.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import NieryhomistPageVideo from '../../images/NieryhomistPageVideo.mp4'
 import css from './NieruchomośćWelcome.module.css'
 
 const NieruchomośćWelcome = () => {
 	return (
 		<section className={css.welcomeSection}>
 			<h1 className={css.title}>Sprzedaż nieruchomości</h1>
+			<video
+				className={css.welcomeVideo}
+				autoPlay
+				loop
+				muted
+				playsInline
+				src={NieryhomistPageVideo}
+			/>
+			<div className={css.welcomeContainer}>
+				<div className={css.specificationItemDiv}></div>
+				<div className={css.specificationContainerText}>
+					<p className={css.specificationItemTextCenter}>
+						Firma Havrysh3 Sp. z o.o. oferuje na sprzedaż nieruchomości o
+						zróżnicowanym charakterze :<br />
+						<strong>
+							-budynek jednorodzinny wolnostojący;
+							<br />
+							-budynek mieszkalno-usługowy;
+							<br />
+							-budynki jednorodzinne dwulokalowe.
+						</strong>
+					</p>
+				</div>
+			</div>
 		</section>
 	)
 }
