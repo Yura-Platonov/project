d87dc991bf916b997c5b7f16ff8159789afe6324
Yura <yuraplatonov2000@gmail.com>
2024-09-20 11:55:10 +0300

    add video

--- src/components/DomyWelcome/DomyWelcome.jsx
+++ src/components/DomyWelcome/DomyWelcome.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import MainPageVideo from '../../images/MainPageVideo.mp4'
 import css from './DomyWelcome.module.css'
 
 const DomyWelcome = () => {
-	return <section className={css.welcomeSection}></section>
+	return (
+		<section className={css.welcomeSection}>
+			<video
+				className={css.welcomeVideo}
+				autoPlay
+				loop
+				muted
+				playsInline
+				src={MainPageVideo}
+			/>
+		</section>
+	)
 }
 
 export default DomyWelcome
--- src/components/NieruchomośćWelcome/NieruchomośćWelcome.jsx
+++ src/components/NieruchomośćWelcome/NieruchomośćWelcome.jsx
@@ -5,11 +5,6 @@ const NieruchomośćWelcome = () => {
 	return (
 		<section className={css.welcomeSection}>
 			<h1 className={css.title}>Sprzedaż nieruchomości</h1>
-
-			{/* <p className={css.text}>Z nami budujesz pewnie!</p>
-			<a href='tel:+48605055346' className={css.textPhone}>
-				+48 605 055 346
-			</a> */}
 		</section>
 	)
 }
