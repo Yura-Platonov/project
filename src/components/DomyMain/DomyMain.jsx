d22f9ea972f02b60370cb29f8a18975173e0de4e
Yura <yuraplatonov2000@gmail.com>
2024-12-09 12:36:05 +0200

    update text

--- src/components/DomyMain/DomyMain.jsx
+++ src/components/DomyMain/DomyMain.jsx
@@ -25,29 +25,46 @@ const DomyMain = () => {
 							/>
 						</Link>
 						<div className={css.textContainer}>
-							<p className={css.textCenter}>
+							{/* <p className={css.textCenter}> */}
+							<p className={css.text}>
 								<span className={css.textAccent}>
 									Firma Havrysh3 Sp. z o.o.
 								</span>{' '}
 								dynamicznie rozwija swoją działalność, realizując inwestycje w
 								zakresie budownictwa mieszkalnego na terenie województw
 								lubuskiego oraz zachodniopomorskiego. Specjalizujemy się w
-								projektach takich jak: domy jednorodzinne wolnostojące, budynki
-								mieszkalno-usługowe oraz dwulokalowe budynki jednorodzinne,
-								dopasowane do indywidualnych potrzeb klientów.
+								projektach takich jak:{' '}
+								<b>
+									domy jednorodzinne wolnostojące, budynki mieszkalno-usługow
+								</b>{' '}
+								oraz <b>dwulokalowe budynki jednorodzinne</b>, dopasowane do
+								indywidualnych potrzeb klientów.
 							</p>
 							<p className={css.text}>
-								Na obrzeżach miasta Gorzów wlkp. W miejscowościach Ulim oraz
-								Santocko realizujemy sześć budynków mieszkalnych dwulokalowych o
-								zróżnicowanej bryle i powierzchni. Na terenie Kamienia
-								Pomorskiego w odległości ok 6km od morza zrealizowany został
-								budynek jednorodzinny z użytkowym poddaszem o wysokim
-								standardzie wykończenia oraz budynek użytkowo-mieszkalny w
-								którym możliwe jest prowadzenie działalności gospodarczej z
-								jednoczesną funkcją mieszkania całorocznego lub wynajmu
-								sezonowego. Dodatkowo planujemy rozpoczęcie realizacji
-								inwestycji o charakterze mieszkaniowym indywidualnym w rejonie
-								Zielonej Góry.
+								W miejscowościach <b>Ulim</b> i <b>Santocko</b>, położonych na
+								obrzeżach <b>Gorzowa Wielkopolskiego</b>, realizujemy aktualnie{' '}
+								<b>sześć budynków mieszkalnych dwulokalowych</b> o nowoczesnej
+								architekturze i zróżnicowanej powierzchni użytkowej. W{' '}
+								<b>Kamieniu Pomorskim</b>, zaledwie 6 km od morza, ukończyliśmy
+								realizację:
+								<ul className={css.workList}>
+									<li className={css.workItem}>
+										<b>Domy jednorodzinne wolnostojące</b>
+									</li>
+									<li className={css.workItem}>
+										<b>Budynki mieszkalno-usługowe</b>
+									</li>
+									<li className={css.workItem}>
+										<b>Dwulokalowe budynki jednorodzinne</b>, w tym bliźniaki
+										domy, dopasowane do indywidualnych potrzeb klientów
+									</li>
+								</ul>
+								Dodatkowo, w najbliższym czasie planujemy rozpoczęcie nowej
+								inwestycji o charakterze mieszkaniowym w rejonie{' '}
+								<b>Zielonej Góry</b>, która również będzie odpowiadała
+								najwyższym standardom jakości i funkcjonalności. Zapraszamy do
+								kontaktu i współpracy przy realizacji wymarzonych projektów
+								mieszkaniowych!
 							</p>
 							<button className={css.buttonDomy}>
 								<Link to='/zabudowania' className={css.relocateLink}>
@@ -73,23 +90,74 @@ const DomyMain = () => {
 						</Link>
 
 						<div className={css.textContainer}>
-							<p className={css.textCenter}>
-								Będąca na rynku od 2018r. Firma Havrysh3 Sp. z o.o. świadczy
-								również usługi w zakresie kompleksowych prac ziemnych, obsługi
-								inwestycji kubaturowych w zakresie pracy sprzętu zapewniającego
-								transport poziomy oraz pionowy do 21m . Prace ziemne prowadzimy
-								samodzielnie pod nadzorem wykwalikowane kadry inżynieryjnej oraz
-								świadczymy usługi wynajmu wraz z operatorem koparek,
-								koparko-ładowarek , ładowarek teleskopowych o wysięgu 17 i 21 m.
-								(JCB 3CX KOPARKO-LADOWARKA 2015, MINIKOPARKA JCB 8020, LADOWARKA
-								TELESKOPOWA JCB 540-170, DIECI PEGASUS 50.21 OBROTOWA, Wozidło
-								budowlane TEREX TA 6 t , MERCEDES ACTROS SAMOCHOD CIEZAROWE Z
-								HDS, Zagęszczarka rewersyjna Ammann APH 5020 DE, Niwelator
-								laserowy NL520R)
-								<br /> <br />
-								Dodatkowo posiadamy środki transportu drogowego umożliwiające
-								przewóz towarów oraz rozładunek o wadzie 22 ton lub przewóz
-								maszyn budowlanych .
+							{/* <p className={css.textCenter}> */}
+							<p className={css.text}>
+								<p>
+									Działająca na rynku od 2018 roku firma{' '}
+									<b>Havrysh3 Sp. z o.o.</b> oferuje kompleksowe usługi w
+									zakresie <b>prac ziemnych</b> oraz obsługi inwestycji
+									kubaturowych na terenie <b>Województwa lubuskiego</b>, miast{' '}
+									<b>Gorzów Wielkopolski</b>, <b>Zielona Góra</b> i okolic.
+								</p>
+								<p>
+									Specjalizujemy się w pracach z użyciem sprzętu zapewniającego
+									transport poziomy oraz pionowy do wysokości 21 m. Wszystkie
+									prace ziemne realizujemy samodzielnie, pod nadzorem wysoko
+									wykwalifikowanej kadry inżynieryjnej, zapewniając najwyższą
+									jakość i terminowość.
+								</p>
+								<br />
+								<p>
+									Nasza oferta obejmuje również{' '}
+									<b>wynajem sprzętu budowlanego wraz z operatoram</b>, w tym:
+								</p>
+
+								<ul className={css.workList}>
+									<li className={css.workItem}>
+										<b>Koparki</b> (np. JCB 3CX, rocznik 2015)
+									</li>
+									<li className={css.workItem}>
+										<b>Minikoparki</b> (np. JCB 8020)
+									</li>
+									<li className={css.workItem}>
+										<b>Ładowarki teleskopowe</b> o wysięgu 17 m i 21 m (JCB
+										540-170, Dieci Pegasus 50.21 Obrotowa)
+									</li>
+									<li className={css.workItem}>
+										<b>Wozidła budowlane</b> (np. Terex TA 6 t)
+									</li>
+									<li className={css.workItem}>
+										<b>Ciężarówki z HDS</b> (np. Mercedes Actros)
+									</li>
+									<li className={css.workItem}>
+										<b>Zagęszczarki rewersyjne</b> (np. Ammann APH 5020 DE)
+									</li>
+									<li className={css.workItem}>
+										<b>Niwelatory laserowe</b> (NL520R)
+									</li>
+								</ul>
+								<p>
+									Ponadto dysponujemy nowoczesnymi środkami transportu
+									drogowego, umożliwiającymi:
+								</p>
+
+								<ul className={css.workList}>
+									<li className={css.workItem}>
+										Przewóz towarów i rozładunek o masie do 22 ton
+									</li>
+									<li className={css.workItem}>Transport maszyn budowlanych</li>
+								</ul>
+								<p>
+									Zapewniamy kompleksową obsługę inwestycji na terenie
+									województw <b>lubuskiego</b> i <b>zachodniopomorskiego</b>,
+									szczególnie w miastach <b>Gorzów Wielkopolski</b>,
+									<b>Zielona Góra</b>, oraz w ich okolicach.{' '}
+									<b>Havrysh3 Sp. z o.o.</b> to niezawodny partner, który
+									sprosta każdemu wyzwaniu w zakresie prac budowlanych i
+									transportowych.
+								</p>
+								<br />
+								<p>Zapraszamy do kontaktu i współpracy!</p>
 							</p>
 							<button className={css.buttonDomy}>
 								<Link to='/technika' className={css.relocateLink}>
@@ -115,30 +183,85 @@ const DomyMain = () => {
 						</Link>
 
 						<div className={css.textContainer}>
-							<p className={css.textCenter}>
-								Firma budowlana Havrysh zaprasza do pracy Ukraińców
-								poszukujących długoterminowej pracy.
-							</p>
-							<p className={css.textNormal}>
-								Oferujemy następujące stanowiska:
-							</p>
-							<ul className={css.workList}>
-								<li className={css.workItem}>Murarze (Муражі) - na akord;</li>
-								<li className={css.workItem}>
-									Zbrojarze (Зброяжі) - na akord;
-								</li>
-								<li className={css.workItem}>Cieśle (Теслярі) - na akord;</li>
-								<li className={css.workItem}>
-									Pracownik ogólnobudowlany na godziny(Різноробочі) ;
-								</li>
-							</ul>
 							<p className={css.text}>
-								Każda brygada jest wyposażona w potrzebne narzędzia do pracy.
-								Poszukujemy pracowników z doświadczeniem w budownictwie, a także
-								młodych, perspektywicznych osób bez doświadczenia. Nasza ekipa
-								składa się z Ukraińców, którzy chętnie pracują i dobrze
-								zarabiają. Pomogą wam szybko odnaleźć się na budowie.
+								<p>
+									Firma budowlana <b>Havrysh</b> zaprasza serdecznie Ukraińców
+									poszukujących <b>stabilnej i długoterminowej pracy</b> do
+									dołączenia do naszego zespołu na terenie{' '}
+									<b>Województwa lubuskiego</b>, <b>Gorzowa Wielkopolskiego</b>,
+									<b>Zielonej Góry</b> oraz okolic.
+								</p>
+								<br />
+								<p>
+									<b>Stanowiska, na które rekrutujemy:</b>
+								</p>
+								<ul className={css.workList}>
+									<li className={css.workItem}>
+										<b>Murarze (Муражі)</b> – praca na akord
+									</li>
+									<li className={css.workItem}>
+										<b>Zbrojarze (Зброяжі)</b> – praca na akord
+									</li>
+									<li className={css.workItem}>
+										<b>Cieśle (Теслярі)</b> – praca na akord
+									</li>
+									<li className={css.workItem}>
+										<b>Pracownicy ogólnobudowlani (Різноробочі)</b> – praca na
+										godziny
+									</li>
+								</ul>
+								<b>Co oferujemy?</b>
+								<ul className={css.workList}>
+									<li className={css.workItem}>
+										<b>Pełne wyposażenie brygad</b> w niezbędne narzędzia
+									</li>
+									<li className={css.workItem}>
+										<b>Atrakcyjne wynagrodzenie</b> adekwatne do umiejętności i
+										zaangażowania
+									</li>
+									<li className={css.workItem}>
+										<b>Stabilne warunki zatrudnienia</b> z możliwością
+										długoterminowej współpracy
+									</li>
+									<li className={css.workItem}>
+										<b>Wsparcie na każdym etapie pracy</b>, od adaptacji na
+										budowie po szkolenia
+									</li>
+								</ul>
+								<b>Kogo szukamy?</b>
+								<ul className={css.workList}>
+									<li className={css.workItem}>
+										<b>Doświadczonych fachowców</b> z branży budowlanej
+									</li>
+									<li className={css.workItem}>
+										<b>Młodych i ambitnych osób</b>, chcących rozpocząć karierę
+										w budownictwie
+									</li>
+								</ul>
+								<p>
+									<b>Dlaczego warto dołączyć do firmy Havrysh?</b>
+									<ul className={css.workList}>
+										<li className={css.workItem}>
+											Nasz zespół składa się głównie z Ukraińców, co ułatwia
+											komunikację i współpracę.
+										</li>
+										<li className={css.workItem}>
+											Oferujemy <b>przyjazną atmosferę pracy</b>, w której
+											doświadczeni pracownicy chętnie dzielą się swoją wiedzą,
+										</li>
+										<li className={css.workItem}>
+											Projekty realizowane są w dynamicznie rozwijających się
+											miastach: <b>Gorzów Wielkopolski</b>, <b>Zielona Góra</b>{' '}
+											oraz okolicach.
+										</li>
+									</ul>
+								</p>
+								<p>
+									Nie czekaj – <b>zbuduj swoją przyszłość</b> razem z nami!
+									Skontaktuj się już dziś i dołącz do zespołu <b>Havrysh</b>
+								</p>
 							</p>
+
 							<button className={css.buttonDomy}>
 								<Link to='/praca' className={css.relocateLink}>
 									Pokaż więcej
